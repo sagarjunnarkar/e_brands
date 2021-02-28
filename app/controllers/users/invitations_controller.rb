a80fb217949f5ff308ef12876529216669564308
sagarjunnarkar <sagarjunnu@gmail.com>
2021-02-28 22:17:59 +0530

    fix: authentication skipped for login url

--- app/controllers/users/invitations_controller.rb
+++ app/controllers/users/invitations_controller.rb
@@ -1,12 +1,12 @@
 class Users::InvitationsController < Devise::InvitationsController
   skip_before_action :verify_authenticity_token, only: [ :create, :update ]
-
+  skip_before_action :authenticate_user, only: [:create, :update]
   def create
     authorize! :invite, current_user
 
     super do |resource|
       if resource.errors.empty?
-        render json: { status: "Invitation Accepted!" }, status: 200 and return
+        render json: { status: "Invitation Sent!" }, status: 200 and return
       else
         render json: resource.errors, status: 401 and return
       end
